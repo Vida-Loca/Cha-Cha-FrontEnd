0d08887b51e925b65749d27e24056c7b6a8a0efc
DaRoTP <DRodzewicz@gmail.com>
2019-12-19 23:08:41 +0200

    minor fixes, -added id to supplyTiles -fixed PropTYpes validation error

--- src/layout/EventTabs/Supply/Data/TempData.js
+++ src/layout/EventTabs/Supply/Data/TempData.js
@@ -1,41 +1,105 @@
 const SupplyContainers = [
   {
     Category: "Drinks",
-    showMore: false,
     supplies: [
       {
+        id: "dkk43",
         supply:
           "hortex juice with a salty hortex juice with a salty taste for not to much",
         user: "Lukas200K",
         quantity: 2,
         price: 5
       },
-      { supply: "beer", user: "Gorgios", quantity: 1, price: 5 },
-      { supply: "muler michkskd", user: "Jankek", quantity: 2, price: 5 },
-      { supply: "Coke", user: "Janek", quantity: 3, price: 10 },
-      { supply: "hortex juice", user: "Lukas", quantity: 2, price: 5 }
+      { id: "fef44gg", supply: "beer", user: "Gorgios", quantity: 1, price: 5 },
+      {
+        id: "htggh",
+        supply: "muler michkskd",
+        user: "Jankek",
+        quantity: 2,
+        price: 5
+      },
+      {
+        id: "htjkk57hh",
+        supply: "Coke",
+        user: "Janek",
+        quantity: 3,
+        price: 10
+      },
+      {
+        id: "gggRRTth",
+        supply: "hortex juice",
+        user: "Lukas",
+        quantity: 2,
+        price: 5
+      }
     ]
   },
   {
     Category: "Food",
-    showMore: false,
     supplies: [
-      { supply: "hortex juice", user: "Lukas", quantity: 2, price: 5 },
-      { supply: "beer", user: "Gorgios", quantity: 1, price: 5 },
-      { supply: "muler michkskd", user: "Jankek", quantity: 2, price: 5 },
-      { supply: "Coke", user: "Janek", quantity: 3, price: 1000005 },
-      { supply: "hortex juice", user: "Lukas", quantity: 2, price: 5 }
+      {
+        id: "fe677fef",
+        supply: "hortex juice",
+        user: "Lukas",
+        quantity: 2,
+        price: 5
+      },
+      { id: "grRrrr4", supply: "beer", user: "Gorgios", quantity: 1, price: 5 },
+      {
+        id: "jgygyyRR",
+        supply: "muler michkskd",
+        user: "Jankek",
+        quantity: 2,
+        price: 5
+      },
+      {
+        id: "grgf5gfg",
+        supply: "Coke",
+        user: "Janek",
+        quantity: 3,
+        price: 1000005
+      },
+      {
+        id: "ggGGddw556",
+        supply: "hortex juice",
+        user: "Lukas",
+        quantity: 2,
+        price: 5
+      }
     ]
   },
   {
     Category: "Party Items",
-    showMore: false,
     supplies: [
-      { supply: "hortex juice", user: "Lukas", quantity: 2, price: 5 },
-      { supply: "beer", user: "Gorgios", quantity: 1, price: 5 },
-      { supply: "muler michkskd", user: "Jankek", quantity: 2, price: 5 },
-      { supply: "Coke", user: "Janek", quantity: 32, price: 10 },
-      { supply: "hortex juice", user: "Lukas", quantity: 2, price: 5 }
+      {
+        id: "fefe45ff",
+        supply: "hortex juice",
+        user: "Lukas",
+        quantity: 2,
+        price: 5
+      },
+      { id: "444ff", supply: "beer", user: "Gorgios", quantity: 1, price: 5 },
+      {
+        id: "hhtDD",
+        supply: "muler michkskd",
+        user: "Jankek",
+        quantity: 2,
+        price: 5
+      },
+      {
+        id: "dd4ggTTT",
+        supply: "Coke",
+        user: "Janek",
+        quantity: 32,
+        price: 10
+      },
+      {
+        id: "fefe56f",
+        supply: "hortex juice",
+        user: "Lukas",
+        quantity: 2,
+        price: 5
+      }
     ]
   }
 ];
