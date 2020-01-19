d9ecc8e566a5a61352534aa770ba5c84fa4f89b4
Darek <DRodzewicz@gmail.com>
2020-01-19 13:54:59 +0100

    editing supply

--- src/components/SupplyUserTile/SupplyUserTile.js
+++ src/components/SupplyUserTile/SupplyUserTile.js
@@ -4,12 +4,34 @@ import Avatar from "../Avatar/Avatar";
 import ThreeDots from "../ThreeDots/ThreeDots";
 import Button from "../button/Button";
 import "./SupplyUserTile.scss";
+import Form from "../Form/Form";
+import TextInput from "../Inputs/TextInput/TextInput";
 
 const SupplyUserTile = ({ user, supply, price }) => {
+  const [tileSupply, setTileSuply] = useState({
+    user: user,
+    supply: supply,
+    price: price
+  });
+
+  const [editState, setEditState] = useState({ edit: false });
+
   const [tileState, tileStateSet] = useState({ options: false });
 
   const changeOptions = () => {
     tileStateSet({ options: !tileState.options });
+  };
+
+  const onChangeHandler = event => {
+    setTileSuply({
+      ...tileSupply,
+      [`${event.target.name}`]: event.target.value
+    });
+    console.log(tileSupply);
+  };
+
+  const editHandler = () => {
+    setEditState({ edit: !editState.edit });
   };
 
   return (
@@ -18,24 +40,52 @@ const SupplyUserTile = ({ user, supply, price }) => {
         <div>
           <Avatar imageLink="https://basicincome.org/wp-content/uploads/2018/12/profilepic.jpg" />
           <span className="SuplyNameLabel">
-            <strong>
-              {price}
-              zl -{" "}
-            </strong>
-            {supply}
+            {!editState.edit ? (
+              <span>
+                <strong>
+                  {tileSupply.price}
+                  zl -{" "}
+                </strong>
+                {tileSupply.supply}
+              </span>
+            ) : (
+              <span>
+                <TextInput
+                  onChange={onChangeHandler}
+                  placeholder="Price"
+                  name="price"
+                />
+                <TextInput
+                  onChange={onChangeHandler}
+                  placeholder="Supply name"
+                  name="supply"
+                />
+              </span>
+            )}
           </span>
         </div>
         <ThreeDots clicked={changeOptions} />
       </div>
       {tileState.options ? (
-        <div className="Options">
-          <Button classes="btn-sm btn-blueGradient">
-            <i className="far fa-edit" />
-          </Button>
-          <Button classes="btn-sm btn-orangeGradient">
-            <i className="far fa-trash-alt" />
-          </Button>
-        </div>
+        editState.edit ? (
+          <div className="Options">
+            <Button clicked={editHandler} classes="btn-sm btn-orangeGradient">
+              <i className="fas fa-times-circle" />
+            </Button>
+            <Button classes="btn-sm btn-orangeGradient">
+              <i className="fas fa-check" />
+            </Button>
+          </div>
+        ) : (
+          <div className="Options">
+            <Button clicked={editHandler} classes="btn-sm btn-blueGradient">
+              <i className="far fa-edit" />
+            </Button>
+            <Button classes="btn-sm btn-orangeGradient">
+              <i className="far fa-trash-alt" />
+            </Button>
+          </div>
+        )
       ) : null}
     </div>
   );
