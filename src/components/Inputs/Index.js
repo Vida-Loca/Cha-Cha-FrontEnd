9b11553505dc74c9867d5318128cd1be61dd00cb
Darek <DRodzewicz@gmail.com>
2020-05-04 21:25:06 +0200

    fix 1/2

--- src/components/Inputs/Index.js
+++
@@ -1,7 +0,0 @@
-import TextInput from "./TextInput";
-import TextInputNL from "./TextInputNL";
-import TextArea from "./TextArea";
-import SearchBar from "./SearchBar";
-import OptionsInput from "./OptionsInput";
-
-export { SearchBar, TextInput, TextInputNL, TextArea, OptionsInput };
--- src/components/button/Index.js
+++
@@ -1,5 +0,0 @@
-import Button from "./Button";
-import ShowMore from "./ShowMore";
-import EditButton from "./EditButton";
-
-export { Button, ShowMore, EditButton };
